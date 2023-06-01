32e3b1f987b1ae504f25bc1a342b0b20f8dd6d4d
Daw-Ran Liou <hi@dawranliou.com>
2023-06-01 17:53:13 +0800

    Resize window to 640x576

--- conf.lua
+++ conf.lua
@@ -4,8 +4,8 @@ love.conf = function(t)
    t.modules.joystick = false
    t.modules.physics = false
    -- t.window.resizable = true
-   t.window.width = 160 * 3
-   t.window.height = 144 * 3
+   t.window.width = 160 * 4
+   t.window.height = 144 * 4
    t.window.vsync = true
    t.version = "11.4"
 end
